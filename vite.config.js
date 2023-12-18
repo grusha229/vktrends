232e9b63eef047bccfc7983568189fa907fc45bb
Grigory Tupikov <gtupikov@kabanov.agency>
2023-12-18 23:09:13 +0300

    desktop md&s

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import styleX from "vite-plugin-stylex";
-import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
-  plugins: [react(), styleX(),svgLoader()],
+  plugins: [react(), styleX()],
 });
